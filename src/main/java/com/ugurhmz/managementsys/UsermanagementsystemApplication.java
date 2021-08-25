c9f38ea534460d8dad6eb775e3fad7e8be2e156f
Ugur hamzaoglu <craxx67@gmail.com>
2021-08-25 23:52:32 +0300

    First commit

---
+++ src/main/java/com/ugurhmz/managementsys/UsermanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.ugurhmz.managementsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ugurhmz/managementsys/UsermanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugurhmz.managementsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
