f6c543d5f520144148a7b12e1c2c46202c6b2d28
Ugur hamzaoglu <craxx67@gmail.com>
2021-08-26 02:36:12 +0300

    Other Roles Tested JUnit, added db

--- src/test/java/com/ugurhmz/managementsys/repositories/RoleRepositoryTests.java
+++ src/test/java/com/ugurhmz/managementsys/repositories/RoleRepositoryTests.java
@@ -2,6 +2,7 @@ package com.ugurhmz.managementsys.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,25 @@ public class RoleRepositoryTests {
 		Role savedAdminRole = roleRepository.save(adminRole);
 		
 		assertThat(savedAdminRole.getId()).isGreaterThan(0);
+	}
+	
+	
+	@Test
+	public void testCreateRestRoles() {
+		Role salesPersonRole = new Role("SalesPerson","I'm Sales Person   I  Manage product price, customer, shipping, orders and sales report"); 
+		
+		Role editorRole = new Role("Editor","I'm Editor, I  manage categories"
+				+ ", brands, products, articles and menus");
+		
+		Role shipperRole = new Role("Shipper", "I'm Shipper, I manage view products"
+				+ ", view orders");
+	
+		Role assistantRole = new Role("Assistant", "I'm Assistant, I manage questions and reviews");
+		
+		
+		roleRepository.saveAll(List.of(salesPersonRole, editorRole, shipperRole, assistantRole));
+		
+		
 	}
 	
 }
