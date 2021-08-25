eb0f9d0a461c3a785f1be76d9dae14972c973de7
Ugur hamzaoglu <craxx67@gmail.com>
2021-08-26 00:37:27 +0300

    User, Role Entity classes created

---
+++ src/main/java/com/ugurhmz/managementsys/entity/Role.java
@@ -0,0 +1,100 @@
+package com.ugurhmz.managementsys.entity;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="Roles")
+public class Role {
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = true, length= 50, unique= true  )
+	private String name;
+	
+	@Column(length= 200, nullable=true)
+	private String description;
+	
+	
+	// CONSTRUCTORS
+	
+	public Role() {
+		
+	}
+	
+	public Role(Integer id) {
+		this.id = id;
+	}
+	
+	
+	public Role(String name) {
+		this.name = name;
+		
+	}
+	
+	public Role(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+	
+	
+	// GETTER & SETTER
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+
+	
+	//hashCode & equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/ugurhmz/managementsys/entity/User.java
@@ -0,0 +1,163 @@
+package com.ugurhmz.managementsys.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+
+
+@Entity
+@Table(name="Users")
+public class User {
+
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="first_name", nullable=false, length=80)
+	private String firstName;
+	
+	@Column(name="last_name", nullable=false, length=120)
+	private String lastName;
+	
+	@Column(nullable=false, length=200, unique=true)
+	private String email;
+	
+	@Column(nullable=false, length=80)
+	private String password;
+	
+	@Column(length=100)
+	private String photos;
+	
+	
+	private boolean enabled;
+	
+	
+	
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+				name = "users_roles",
+				joinColumns = @JoinColumn(name="user_id"),
+				inverseJoinColumns = @JoinColumn(name="role_id")
+			)
+	private Set<Role> roles = new HashSet<>();
+
+
+	
+	
+	// CONSTRUCTORS 
+	public User() {
+		
+	}
+	
+	
+	public User(String firstName, String lastName, String email, String password) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+
+
+	// GETTER & SETTER
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getPhotos() {
+		return photos;
+	}
+
+
+	public void setPhotos(String photos) {
+		this.photos = photos;
+	}
+
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+
+	// toString
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", photos=" + photos + ", enabled=" + enabled + ", roles=" + roles + "]";
+	}
+	
+}
