822fe75867b499dec91d993516cdc552a7530fd2
Ugur hamzaoglu <craxx67@gmail.com>
2021-08-26 21:09:29 +0300

    User added success without image & encode pw in db

--- src/main/java/com/ugurhmz/managementsys/controller/UserController.java
+++ src/main/java/com/ugurhmz/managementsys/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ugurhmz.managementsys.entity.Role;
 import com.ugurhmz.managementsys.entity.User;
@@ -38,6 +40,8 @@ public class UserController {
 	}
 	
 	
+	
+	
 	// GET NEW USER FORM
 	@GetMapping("/users/new-user")
 	public String getNewUser(Model model) {
@@ -55,6 +59,24 @@ public class UserController {
 		
 		return "newUserForm";
 	}
+	
+	
+	// POST NEW USER
+	@PostMapping("/users/save") 	// It'll be same in action  th:action="@{/users/save}
+	public String postNewUser(
+			User user, 
+			RedirectAttributes redirectAttributes
+			) 
+	{
+		
+		userService.save(user);
+		
+		
+		return "redirect:/users";
+	}
+	
+	
+	
 	
 	
 	
